2c7998c90e4273f367f75d8656e9d331b3051870
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-09-20 10:22:21 -0300

    criando elementos com JS , para a tag UL

--- js/main.js
+++ js/main.js
@@ -1,10 +1,44 @@
 const form = document.getElementById ('novoItem');
+const lista = document.getElementById ('lista');
+
+
+
+
 
 //console.log (novoItem);
 
 form.addEventListener ('submit', (evento) =>{
          evento.preventDefault ();
-         console.log ("funcionou");
-         console.log (evento.target.elements['nome'].value);
-         console.log (evento.target.elements['quantidade'].value);
+         //console.log ("funcionou");
+         
+
+
+         criaElemento (evento.target.elements['nome'].value, evento.target.elements['quantidade'].value);
 });
+
+
+
+
+function criaElemento(nome,quantidade) {
+    //console.log (nome);
+    //console.log (quantidade);
+
+
+    const novoItem = document.createElement ('li');
+    novoItem.classList.add ("item");
+
+
+    const numeroItem = document.createElement ('strong');
+    numeroItem.innerHTML = quantidade;
+
+   // console.log (numeroItem)
+
+
+   novoItem.appendChild (numeroItem);
+   novoItem.innerHTML += nome;
+     //console.log (novoItem)
+   
+
+     
+     lista.appendChild (novoItem);
+}
